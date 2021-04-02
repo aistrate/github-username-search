5dcbc9d8865a9eb4e21d8ac0667066f7710b8115
Adrian Istrate <adr_istrate@yahoo.com>
2021-04-02 21:27:52 +0200

    Split App component into 3: Header, Navigation and Content.

--- src/App.tsx
+++ src/App.tsx
@@ -16,41 +16,57 @@ const appName = "GitHub Username Search";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/search" activeStyle={{ fontWeight: "bold" }}>
-                Search
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/history" activeStyle={{ fontWeight: "bold" }}>
-                History
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+    <div className="App">
+      <Router>
+        <Header />
+        <Navigation />
+        <Content />
+      </Router>
+    </div>
+  );
+}
 
-        <Switch>
-          <Route path="/search">
-            <Helmet>
-              <title>Search - {appName}</title>
-            </Helmet>
-            <Search />
-          </Route>
-          <Route path="/history">
-            <Helmet>
-              <title>History - {appName}</title>
-            </Helmet>
-            <History />
-          </Route>
-          <Route path="*">
-            <Redirect to="/search" />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+function Header() {
+  return <div>{appName}</div>;
+}
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/search" activeStyle={{ fontWeight: "bold" }}>
+            Search
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/history" activeStyle={{ fontWeight: "bold" }}>
+            History
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function Content() {
+  return (
+    <Switch>
+      <Route path="/search">
+        <Helmet>
+          <title>Search - {appName}</title>
+        </Helmet>
+        <Search />
+      </Route>
+      <Route path="/history">
+        <Helmet>
+          <title>History - {appName}</title>
+        </Helmet>
+        <History />
+      </Route>
+      <Route path="*">
+        <Redirect to="/search" />
+      </Route>
+    </Switch>
   );
 }
