996f23f5f188438e2f5b1e8240937c2960b2640d
Adrian Istrate <adr_istrate@yahoo.com>
2021-04-08 04:12:05 +0200

    Comment out unused code (which would cause eslint warnings, which would break the Netlify build).

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,40 +1,40 @@
 import { useState } from "react";
-import type { User, Repo } from "./Models";
+import type { User } from "./Models";
 import type { SearchEvent } from "./SearchForm";
 import SearchForm from "./SearchForm";
 import UserInfo from "./UserInfo";
-import RepoList from "./RepoInfo";
+// import RepoList from "./RepoInfo";
 
 export default SearchPage;
 
-const repoData: Repo[] = [
-  {
-    id: 10270250,
-    name: "react",
-    html_url: "https://github.com/facebook/react",
-    fork: false,
-    description:
-      "A declarative, efficient, and flexible JavaScript library for building user interfaces.",
-    homepage: "https://reactjs.org",
-    language: "JavaScript",
-    stargazers_count: 166391,
-    forks_count: 33414,
-    updated_at: "2021-04-04T20:01:12Z",
-  },
-  {
-    id: 40508605,
-    name: "relay",
-    html_url: "https://github.com/facebook/relay",
-    fork: false,
-    description:
-      "Relay is a JavaScript framework for building data-driven React applications.",
-    homepage: "https://relay.dev",
-    language: "JavaScript",
-    stargazers_count: 15533,
-    forks_count: 33414,
-    updated_at: "2021-04-04T09:00:37Z",
-  },
-];
+// const repoData: Repo[] = [
+//   {
+//     id: 10270250,
+//     name: "react",
+//     html_url: "https://github.com/facebook/react",
+//     fork: false,
+//     description:
+//       "A declarative, efficient, and flexible JavaScript library for building user interfaces.",
+//     homepage: "https://reactjs.org",
+//     language: "JavaScript",
+//     stargazers_count: 166391,
+//     forks_count: 33414,
+//     updated_at: "2021-04-04T20:01:12Z",
+//   },
+//   {
+//     id: 40508605,
+//     name: "relay",
+//     html_url: "https://github.com/facebook/relay",
+//     fork: false,
+//     description:
+//       "Relay is a JavaScript framework for building data-driven React applications.",
+//     homepage: "https://relay.dev",
+//     language: "JavaScript",
+//     stargazers_count: 15533,
+//     forks_count: 33414,
+//     updated_at: "2021-04-04T09:00:37Z",
+//   },
+// ];
 
 function SearchPage() {
   const [user, setUser] = useState<User | null>(null);
