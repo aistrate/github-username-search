e4535c80314563a7d2d1202336d22ab9b1beb9d1
Adrian Istrate <adr_istrate@yahoo.com>
2021-04-10 21:35:12 +0200

    Add content on the About page.

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -1,9 +1,35 @@
+import { Link } from "./Styled";
+
 export default AboutPage;
 
 function AboutPage() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <p>
+        Check out the source code for the website{" "}
+        <Link href="https://github.com/aistrate/github-username-search">
+          here
+        </Link>
+        .
+      </p>
+
+      <h3>How to search</h3>
+
+      <p>Search for full usernames, not partials.</p>
+
+      <p>Examples:</p>
+      <ul>
+        <li>
+          <b>Organizations:</b> facebook, google, netflix, apple, amzn, aws,
+          microsoft, github, reddit, twitter, airbnb, shopify, intel, ibm,
+          nodejs, graphql, gatsbyjs
+        </li>
+        <li>
+          <b>Individuals:</b> fabpot, taylorotwell, wycats, mojombo, gaearon,
+          yyx990803, Rich-Harris, rtfeldman, lattner, kentcdodds, shanselman,
+          jskeet, serras
+        </li>
+      </ul>
+    </>
   );
 }
