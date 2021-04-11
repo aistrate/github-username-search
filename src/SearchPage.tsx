6052b8e038aedb958a954f4f18987b952af08a6d
Adrian Istrate <adr_istrate@yahoo.com>
2021-04-11 18:13:31 +0200

    Fetch Repos (first version, copy/paste from User fetch).

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,42 +1,13 @@
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import type { User } from "./Models";
+import type { User, Repo } from "./Models";
 import type { SearchEvent } from "./SearchForm";
 import { Message } from "./Styled";
 import SearchForm from "./SearchForm";
 import UserInfo from "./UserInfo";
-// import RepoList from "./RepoInfo";
+import RepoList from "./RepoInfo";
 
 export default SearchPage;
-
-// const repoData: Repo[] = [
-//   {
-//     id: 10270250,
-//     name: "react",
-//     html_url: "https://github.com/facebook/react",
-//     fork: false,
-//     description:
-//       "A declarative, efficient, and flexible JavaScript library for building user interfaces.",
-//     homepage: "https://reactjs.org",
-//     language: "JavaScript",
-//     stargazers_count: 166391,
-//     forks_count: 33414,
-//     updated_at: "2021-04-04T20:01:12Z",
-//   },
-//   {
-//     id: 40508605,
-//     name: "relay",
-//     html_url: "https://github.com/facebook/relay",
-//     fork: false,
-//     description:
-//       "Relay is a JavaScript framework for building data-driven React applications.",
-//     homepage: "https://relay.dev",
-//     language: "JavaScript",
-//     stargazers_count: 15533,
-//     forks_count: 33414,
-//     updated_at: "2021-04-04T09:00:37Z",
-//   },
-// ];
 
 type SearchPageProps = {
   queryString: string;
@@ -44,6 +15,7 @@ type SearchPageProps = {
 
 function SearchPage({ queryString }: SearchPageProps) {
   const [user, setUser] = useState<User | null>(null);
+  const [repoList, setRepoList] = useState<Repo[]>([]);
   const [error, setError] = useState("");
   const [info, setInfo] = useState("");
 
@@ -56,39 +28,73 @@ function SearchPage({ queryString }: SearchPageProps) {
     setInfo("");
 
     const username = queryString;
+    fetchUser(username);
+    fetchRepoList(username, 1);
+  }, [queryString]);
 
+  async function fetchUser(username: string) {
     if (username === "") {
       setUser(null);
       return;
     }
 
-    (async function () {
-      let response: Response;
-      try {
-        response = await fetch(getUserUrl(username));
-      } catch (err) {
-        const message = (err as Error).message;
-        setError(`Fetch Error: ${message}`);
-        setUser(null);
-        return;
+    let response: Response;
+    try {
+      response = await fetch(getUserUrl(username));
+    } catch (err) {
+      const message = (err as Error).message;
+      setError(`Fetch Error: ${message}`);
+      setUser(null);
+      return;
+    }
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        setInfo(`Username '${username}' was not found.`);
+      } else {
+        let errorData = await response.json();
+        setError(`HTTP Error: (${response.status}) ${errorData.message}`);
       }
 
-      if (!response.ok) {
-        if (response.status === 404) {
-          setInfo(`Username '${username}' was not found.`);
-        } else {
-          let errorData = await response.json();
-          setError(`HTTP Error: (${response.status}) ${errorData.message}`);
-        }
+      setUser(null);
+      return;
+    }
 
-        setUser(null);
-        return;
+    let userData: User = await response.json();
+    setUser(userData);
+  }
+
+  async function fetchRepoList(username: string, page: number) {
+    if (username === "") {
+      setRepoList([]);
+      return;
+    }
+
+    let response: Response;
+    try {
+      response = await fetch(getRepoListUrl(username, page));
+    } catch (err) {
+      const message = (err as Error).message;
+      setError(`Fetch Error: ${message}`);
+      setRepoList([]);
+      return;
+    }
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        setInfo(`Repos for username '${username}' were not found.`);
+      } else {
+        let errorData = await response.json();
+        setError(`HTTP Error: (${response.status}) ${errorData.message}`);
       }
 
-      let userData: User = await response.json();
-      setUser(userData);
-    })();
-  }, [queryString]);
+      setRepoList([]);
+      return;
+    }
+
+    let repoListData: Repo[] = await response.json();
+    setRepoList(repoListData);
+  }
 
   function handleSearch(e: SearchEvent) {
     let username = e.value;
@@ -106,12 +112,15 @@ function SearchPage({ queryString }: SearchPageProps) {
       <Message error={error} info={info} />
 
       {user && <UserInfo user={user} />}
-
-      {/* <RepoList repos={repoData} /> */}
+      {repoList.length > 0 && <RepoList repos={repoList} />}
     </>
   );
 }
 
 function getUserUrl(username: string) {
   return `https://api.github.com/users/${username}`;
+}
+
+function getRepoListUrl(username: string, page: number) {
+  return `https://api.github.com/users/${username}/repos?per_page=100&page=${page}`;
 }
