cf722264b4f773bcc45367eb482fe3bc07027aca
Adrian Istrate <adr_istrate@yahoo.com>
2021-04-20 01:02:50 +0200

    Add styling for page number / page count.

--- src/Search/Pagination.tsx
+++ src/Search/Pagination.tsx
@@ -8,17 +8,19 @@ type PaginationProps = {
 };
 
 function Pagination({ username, page }: PaginationProps) {
+  const pageCount = 10;
+
   return (
     <div className="Pagination">
       <RouterLink
-        className="Link"
+        className="Link Pagination__link"
         to={`/search?q=${username}&page=${page - 1}`}
       >
         &lt; Previous
       </RouterLink>
-
+      &nbsp;&nbsp; Page {page}/{pageCount} &nbsp;&nbsp;
       <RouterLink
-        className="Pagination__next Link"
+        className="Link Pagination__link"
         to={`/search?q=${username}&page=${page + 1}`}
       >
         Next &gt;
