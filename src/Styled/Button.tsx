fb9fb77f3cc261109830aa605877d51681dbeb33
Adrian Istrate <adr_istrate@yahoo.com>
2021-05-02 19:59:20 +0200

    Convert Button component to styled-components.

--- src/Styled/Button.tsx
+++ src/Styled/Button.tsx
@@ -1,7 +1,32 @@
-import styles from "./Button.module.css";
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 600;
+  margin-left: 1em;
+  padding: 0.5em 1em;
+  border: none;
+  border-radius: 6px;
+  background-color: #3078ca;
+  color: white;
+  outline: none;
+
+  :not(:disabled) {
+    cursor: pointer;
+  }
+
+  :hover:not(:disabled) {
+    background-color: #22548e;
+  }
+
+  :disabled {
+    opacity: 0.65;
+  }
+
+  :focus {
+    box-shadow: 0 0 0 2px white, 0 0 0 4px #3078ca;
+  }
+`;
 
 export default Button;
-
-function Button(props: React.ComponentPropsWithoutRef<"button">) {
-  return <button className={styles.Button} {...props} />;
-}
