4bb33cd3a468758033907fc01ffb2c95998eb20c
Adrian Istrate <adr_istrate@yahoo.com>
2021-12-04 21:27:30 +0200

    Add error handling to thunk `fetchUser`.

--- src/features/search/userSlice.ts
+++ src/features/search/userSlice.ts
@@ -1,22 +1,32 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { User } from "./models";
 
-const fetchUser = createAsyncThunk(
-  "user/fetchUser",
-  async (username: string) => {
-    if (!username) {
-      return null;
-    }
-
-    const response = await fetch(getUserUrl(username), fetchOptions);
-    if (response.ok) {
-      const data: User = await response.json();
-      return extractUserFields(data);
-    } else {
-      return null;
-    }
+const fetchUser = createAsyncThunk<
+  { data: User | null; httpStatus: number | null },
+  string,
+  { rejectValue: { errorMessage: string; httpStatus: number | null } }
+>("user/fetchUser", async (username, { rejectWithValue }) => {
+  if (!username) {
+    return { data: null, httpStatus: null };
   }
-);
+
+  let response: Response;
+  try {
+    response = await fetch(getUserUrl(username), fetchOptions);
+  } catch (err) {
+    const errorMessage = `Error: ${(err as Error).message}`;
+    return rejectWithValue({ errorMessage, httpStatus: null });
+  }
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    const errorMessage = `HTTP Error: (${response.status}) ${errorData.message}`;
+    return rejectWithValue({ errorMessage, httpStatus: response.status });
+  }
+
+  const data: User = await response.json();
+  return { data: extractUserFields(data), httpStatus: response.status };
+});
 
 function getUserUrl(username: string) {
   return `https://api.github.com/users/${username}`;
@@ -69,10 +79,20 @@ const userSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchUser.pending, (state) => {
+        state.httpStatus = null;
+        state.error = null;
         state.isLoading = true;
       })
       .addCase(fetchUser.fulfilled, (state, action) => {
-        state.data = action.payload;
+        state.data = action.payload.data;
+        state.httpStatus = action.payload.httpStatus;
+        state.error = null;
+        state.isLoading = false;
+      })
+      .addCase(fetchUser.rejected, (state, action) => {
+        state.data = null;
+        state.httpStatus = action.payload?.httpStatus ?? null;
+        state.error = action.payload?.errorMessage ?? null;
         state.isLoading = false;
       });
   },
