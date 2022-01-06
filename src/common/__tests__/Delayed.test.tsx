1c1eabae5ae60a0976438f01c2abf7585a9f139f
Adrian Istrate <adr_istrate@yahoo.com>
2022-01-06 19:31:21 +0200

    Change assertion to use matching function with more meaningful name.

--- src/common/__tests__/Delayed.test.tsx
+++ src/common/__tests__/Delayed.test.tsx
@@ -5,13 +5,14 @@ test("should render content with delay", () => {
   jest.useFakeTimers();
 
   render(<Delayed delay={1000}>Delayed content</Delayed>);
-  expect(screen.queryByText("Delayed content")).toBeNull();
+
+  expect(screen.queryByText("Delayed content")).not.toBeInTheDocument();
 
   jest.advanceTimersByTime(900);
-  expect(screen.queryByText("Delayed content")).toBeNull();
+  expect(screen.queryByText("Delayed content")).not.toBeInTheDocument();
 
   act(() => {
     jest.advanceTimersByTime(100);
   });
-  expect(screen.queryByText("Delayed content")).not.toBeNull();
+  expect(screen.queryByText("Delayed content")).toBeInTheDocument();
 });
