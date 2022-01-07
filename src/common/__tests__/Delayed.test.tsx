feb859040637b8170d19d5a23e3f45969b97f03c
Adrian Istrate <adr_istrate@yahoo.com>
2022-01-07 15:43:15 +0200

    Make test assertion more general.

--- src/common/__tests__/Delayed.test.tsx
+++ src/common/__tests__/Delayed.test.tsx
@@ -6,13 +6,13 @@ test("should render content with delay", () => {
 
   render(<Delayed delay={1000}>Delayed content</Delayed>);
 
-  expect(screen.queryByText("Delayed content")).not.toBeInTheDocument();
+  expect(screen.queryByText(/delayed content/i)).not.toBeInTheDocument();
 
   jest.advanceTimersByTime(900);
-  expect(screen.queryByText("Delayed content")).not.toBeInTheDocument();
+  expect(screen.queryByText(/delayed content/i)).not.toBeInTheDocument();
 
   act(() => {
     jest.advanceTimersByTime(100);
   });
-  expect(screen.queryByText("Delayed content")).toBeInTheDocument();
+  expect(screen.queryByText(/delayed content/i)).toBeInTheDocument();
 });
