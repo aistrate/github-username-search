ca488128912184977de49b3e137716c3fe5708e2
Adrian Istrate <adr_istrate@yahoo.com>
2022-01-10 16:11:11 +0200

    Make the mock repos handler behave similar to the real GitHub API (for pages < 1 or > max page).

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,14 +6,20 @@ const handlers = [
   rest.get(`${baseUrl}/users/:username`, (req, res, ctx) => {
     const username = req.params["username"] as string;
 
-    return res(ctx.json(mockUsers[username].user));
+    const user = mockUsers[username].user;
+
+    return res(ctx.json(user));
   }),
 
   rest.get(`${baseUrl}/users/:username/repos`, (req, res, ctx) => {
     const username = req.params["username"] as string;
-    const page = parseInt(req.url.searchParams.get("page") || "1");
 
-    return res(ctx.json(mockUsers[username].repoPages[page]));
+    let page = parseInt(req.url.searchParams.get("page") || "1");
+    page = Math.max(1, page);
+
+    const repoPage = mockUsers[username].repoPages[page] || [];
+
+    return res(ctx.json(repoPage));
   }),
 ];
 
