f26e342d9b296a3ddd5dd1ad05a080c961d35999
Adrian Istrate <adr_istrate@yahoo.com>
2022-01-10 18:57:50 +0200

    Comment out the lines that enable client-side mocking in development.

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./app/App";
 import "./index.css";
 
-if (process.env.NODE_ENV === "development") {
-  (async () => {
-    const { worker } = await import("./mocks/browser");
-    worker.start({ onUnhandledRequest: "bypass" });
-  })();
-}
+// Uncomment the following lines to enable client-side mocking in development
+// if (process.env.NODE_ENV === "development") {
+//   (async () => {
+//     const { worker } = await import("./mocks/browser");
+//     worker.start({ onUnhandledRequest: "bypass" });
+//   })();
+// }
 
 ReactDOM.render(
   <React.StrictMode>
