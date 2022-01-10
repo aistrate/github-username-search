e52df6530a7f081b4355baf77aa4a8b2ea523e4b
Adrian Istrate <adr_istrate@yahoo.com>
2022-01-10 18:46:33 +0200

    Enable client-side mocking (in the browser), in development only.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./app/App";
 import "./index.css";
 
+if (process.env.NODE_ENV === "development") {
+  (async () => {
+    const { worker } = await import("./mocks/browser");
+    worker.start({ onUnhandledRequest: "bypass" });
+  })();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
