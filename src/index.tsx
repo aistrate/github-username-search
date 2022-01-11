17f195733f097099fccbfd77f22d44eaf67abe3d
Adrian Istrate <adr_istrate@yahoo.com>
2022-01-11 14:53:40 +0200

    When client-side mocking is enabled and a URL like "/search?username=microsoft" is requested from the address bar, the mocked API should be used instead of the real one.

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./app/App";
 import "./index.css";
 
-// Uncomment the following lines to enable client-side mocking in development
-// if (process.env.NODE_ENV === "development") {
-//   (async () => {
-//     const { worker } = await import("./mocks/browser");
-//     worker.start({ onUnhandledRequest: "bypass" });
-//   })();
-// }
+(async () => {
+  // Uncomment the following lines to enable client-side mocking
+  // if (process.env.NODE_ENV === "development") {
+  //   const { worker } = await import("./mocks/browser");
+  //   worker.start({ onUnhandledRequest: "bypass" });
+  // }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+})();
