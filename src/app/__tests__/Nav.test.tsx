494b56fdefd0833bcdc4ab17c446c12990267283
Adrian Istrate <adr_istrate@yahoo.com>
2022-01-21 15:53:17 +0200

    Remove use of testid 'pageContainer' from main menu navigation test.

--- src/app/__tests__/Nav.test.tsx
+++ src/app/__tests__/Nav.test.tsx
@@ -1,27 +1,21 @@
-import {
-  getByText,
-  queryByRole,
-  queryByText,
-  screen,
-} from "@testing-library/react";
+import { getByText, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { renderWithWrapper } from "../../common/testUtils";
 import App from "../App";
 
-test("main menu navigation works correctly", () => {
+test.only("main menu navigation works correctly", () => {
   renderWithWrapper(<App />);
 
-  const menu = screen.getByRole("navigation");
-  const page = screen.getByTestId("pageContainer");
+  expect(screen.queryByRole("button", { name: "Search" })).toBeInTheDocument();
 
-  expect(queryByRole(page, "button", { name: "Search" })).toBeInTheDocument();
+  const menu = screen.getByRole("navigation");
 
   userEvent.click(getByText(menu, "History"));
-  expect(queryByText(page, "History (0)")).toBeInTheDocument();
+  expect(screen.queryByText("History (0)")).toBeInTheDocument();
 
   userEvent.click(getByText(menu, "About"));
-  expect(queryByText(page, "How to search")).toBeInTheDocument();
+  expect(screen.queryByText("How to search")).toBeInTheDocument();
 
   userEvent.click(getByText(menu, "Search"));
-  expect(queryByRole(page, "button", { name: "Search" })).toBeInTheDocument();
+  expect(screen.queryByRole("button", { name: "Search" })).toBeInTheDocument();
 });
