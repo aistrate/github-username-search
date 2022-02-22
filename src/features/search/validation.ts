b35625cbfd9c85a939b05f1f66dda03f7b545f2b
Adrian Istrate <adr_istrate@yahoo.com>
2022-02-22 17:29:28 +0200

    Eliminate variable reassignment.

--- src/features/search/validation.ts
+++ src/features/search/validation.ts
@@ -1,9 +1,7 @@
 export { usernameValidationError };
 
 function usernameValidationError(username: string) {
-  username = username.trim();
-
-  if (username.search(invalidUsernameChars) >= 0) {
+  if (username.trim().search(invalidUsernameChars) >= 0) {
     return (
       "Username may only contain alphanumeric characters or single hyphens, " +
       "and cannot begin or end with a hyphen."
