bd14499764db49c569616274b3fab858dd6ca5cc
Adrian Istrate <adr_istrate@yahoo.com>
2022-02-24 20:34:41 +0200

    Simplify exports.

--- src/testUtils/searchPage.ts
+++ src/testUtils/searchPage.ts
@@ -1,32 +1,24 @@
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-export {
-  clearUsername,
-  searchForUsername,
-  typeUsername,
-  usernameInputValue,
-  waitForUserInfo,
-};
-
-function searchForUsername(username: string) {
+export function searchForUsername(username: string) {
   typeUsername(username);
   userEvent.click(searchButton());
 }
 
-function typeUsername(username: string) {
+export function typeUsername(username: string) {
   userEvent.type(usernameInput(), username);
 }
 
-function clearUsername() {
+export function clearUsername() {
   userEvent.clear(usernameInput());
 }
 
-function usernameInputValue() {
+export function usernameInputValue() {
   return usernameInput().getAttribute("value");
 }
 
-async function waitForUserInfo() {
+export async function waitForUserInfo() {
   await screen.findByTestId("userInfo");
 }
 
